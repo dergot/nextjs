1a3b85c5fdda3fec6eb53fab7aa44e8d01d0001e
Martin <pukiyolo@gmail.com>
2020-06-12 13:27:11 +0300

    link

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,9 @@ export default function Home() {
 					(This is a sample website - you’ll be building a site like this on{" "}
 					<a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
 				</p>
+				<p>
+					<Link href="./posts/first-post"></Link>
+				</p>
 			</section>
 		</Layout>
 	);
